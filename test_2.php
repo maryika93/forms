f36097c4ca0d0175f4419474d97867ae1c45da2a
Maria <maryika93@mail.ru>
2018-03-15 18:38:23 +0600

    forms

--- test_2.php
+++ test_2.php
@@ -9,28 +9,25 @@ if(!isset($_GET['name']) || !file_exists('./tests/' . $_GET['name'])){
 else {
     $tess = file_get_contents('./tests/' . $_GET['name']);
     $test = json_decode($tess, true);
+
     if (isset($test[0]['crc']) && $test[0]['crc'] == "tipikinatestsonlycanberead") {
+
+
         if (!empty($_POST)) {
-            $answer0 = $_POST['answer0'];
-            $answer1 = $_POST['answer1'];
-            $answer2 = $_POST['answer2'];
-            $answer3 = $_POST['answer3'];
             $result  = 0;
-            /*проверка ответов с правильными ответами*/
-            if ($answer0 == $test[1]['true']) {
+            //foreach ($test as $testss):
+                //echo ($testss['true']);
+               /* $answer0 = $_POST['answer0'];
+                $answer1 = $_POST['answer1'];
+                $answer2 = $_POST['answer2'];
+                $answer3 = $_POST['answer3'];*/
+
+           /* if ($answer0 == $testss['true']) {
                 $result += 25;
-            }
-            if ($answer1 == $test[2]['true']) {
-                $result += 25;
-            }
-            if ($answer2 == $test[3]['true']) {
-                $result += 25;
-            }
-            if ($answer3 == $test[4]['true']) {
-                $result += 25;
-            }
+            }*/
             echo "Вы прошли тест на <strong>$result%</strong>";
             die;
+            //endforeach;
         }
     }
     else {
@@ -39,16 +36,13 @@ else {
     }
 }
 ?>
-var_dump($test);
-<?php foreach ($test as $tests):
-var_dump($tests);
-?>
+<?php foreach ($test as $tests):?>
 
 <form method="post" action="http://university.netology.ru/u/mtipikina/forms/test_2.php?name=<?=$_GET['name']?>">
     <p><b><?php echo $tests['question'] ?> </b><Br/>
-        <input type="radio" name="answer1" value=<?= $tests['answer1'] ?>> <?= $tests['answer1'] ?><Br/>
-        <input type="radio" name="answer2" value=<?= $tests['answer2'] ?>> <?= $tests['answer2'] ?><Br/>
-        <input type="radio" name="answer3" value=<?= $tests['answer3'] ?>> <?= $tests['answer3'] ?>
+        <input type="radio" name=<?= $tests['answer1'] ?> value=<?= $tests['answer1'] ?>> <?= $tests['answer1'] ?><Br/>
+        <input type="radio" name=<?= $tests['answer2'] ?> value=<?= $tests['answer2'] ?>> <?= $tests['answer2'] ?><Br/>
+        <input type="radio" name=<?= $tests['answer3'] ?> value=<?= $tests['answer3'] ?>> <?= $tests['answer3'] ?>
     </p>
 <?php endforeach; ?>
     <input type="submit" name="ready" value="Готово">
